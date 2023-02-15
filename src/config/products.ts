d18748591f0ce8bc1cde7771559757bfc78a2847
Odenir Gomes <odenirdev@gmail.com>
2023-02-15 20:30:23 -0300

    Atualizado as artes

--- src/config/products.ts
+++ src/config/products.ts
@@ -4,41 +4,46 @@ export const products: IProductsProps[] = [
   {
     id: "7",
     image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1073667739887079455/PRONTO_64.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541973752094830/68.webp",
   },
   {
     id: "4",
     image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1073666654258606242/PRONTO_66.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541974158934077/40.webp",
   },
   {
     id: "5",
     image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1073665941709271070/PRONTO_65.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541974494498916/61.webp",
   },
   {
     id: "6",
     image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1073665357857947808/PRONTO_67.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541974880362577/62.webp",
   },
   {
     id: "8",
     image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1073663317224206406/PRONTO_61.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541975236870234/67.webp",
   },
   {
     id: "9",
     image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1073663316783800330/PRONTO_59.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075542207290945587/108.webp",
   },
   {
     id: "10",
     image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1073663316238553128/PRONTO_58.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075542417840820336/369.webp",
   },
   {
     id: "1",
     image:
-      "https://cdn.discordapp.com/attachments/770044750459699200/1073284476639129620/insta_pic_1.png",
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075559339659624528/25.webp",
+  },
+  {
+    id: "11",
+    image:
+      "https://cdn.discordapp.com/attachments/1055866913781198891/1075559340020346972/32.webp",
   },
 ];
