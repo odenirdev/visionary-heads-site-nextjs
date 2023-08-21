a3bbbd49c67ff0b457da6fbf9cf729a60eff957d
Odenir Gomes <odenirdev@gmail.com>
2023-08-21 14:58:16 -0300

    fix: Imagens NFTs

--- src/config/products.ts
+++ src/config/products.ts
@@ -3,47 +3,22 @@ import { IProductsProps } from "@/components/Product";
 export const products: IProductsProps[] = [
   {
     id: "7",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541973752094830/68.webp",
+    image: "/a073a47bdb068b71921ddc14fe79f9ce.webp",
   },
   {
     id: "4",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541974158934077/40.webp",
+    image: "/1.webp",
   },
   {
     id: "5",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541974494498916/61.webp",
+    image: "/3a7c96bf8f159f7a3ef20ca727f6ac08.webp",
   },
   {
     id: "6",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541974880362577/62.webp",
+    image: "/032e11da15facc6ca9031681514f8228.webp",
   },
   {
     id: "8",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075541975236870234/67.webp",
-  },
-  {
-    id: "9",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075542207290945587/108.webp",
-  },
-  {
-    id: "10",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075542417840820336/369.webp",
-  },
-  {
-    id: "1",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075559339659624528/25.webp",
-  },
-  {
-    id: "11",
-    image:
-      "https://cdn.discordapp.com/attachments/1055866913781198891/1075559340020346972/32.webp",
+    image: "/67cbd6fd0db180f02b48dcf284713272.webp",
   },
 ];
